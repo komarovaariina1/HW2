c2715614f6bdb09a9afc7a307be8b78402000ac2
Arina <komarovaariina@mail.ru>
2022-09-11 16:45:31 +0300

    task14

---
+++ task14/Program.cs
@@ -0,0 +1,9 @@
+﻿//  Напишите программу, которая принимает на ввод число и проверяет, крастно ли оно одновременно 7 и 23
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int a =7, b =23;
+if (num % a == 0 && num % b == 0) {
+    Console.WriteLine($"Xbckj {num} rhfnyj {a} и {b}");
+
+}
