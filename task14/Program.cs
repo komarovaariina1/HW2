e5dc849444eb903c8551de02e70922d5c9326a8c
Arina <komarovaariina@mail.ru>
2022-09-11 16:49:42 +0300

    task14

--- task14/Program.cs
+++ task14/Program.cs
@@ -4,6 +4,9 @@ Console.WriteLine("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int a =7, b =23;
 if (num % a == 0 && num % b == 0) {
-    Console.WriteLine($"Xbckj {num} rhfnyj {a} и {b}");
+    Console.WriteLine($"Число {num} кратно {a} и {b}");
 
+}
+else {
+    Console.WriteLine($"Число {num} не кратно {a} и {b}");
 }
