8b36843c68c826634e8478a6b390e48ef1706634
Arina <komarovaariina@mail.ru>
2022-09-11 16:25:03 +0300

    task12

---
+++ task12/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Привет, пользователь! Введи первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Отлично! Продолжай, введи второ число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int kako = num1 % num2;
+
+if(num1 > num2){
+    if(kako == 0){
+        Console.WriteLine($"{num2} кратно {num1}");
+    }
+    else{
+        Console.WriteLine($"{num2} не кратен {num1}, остаток = {kako}");
+
+    }
+    Console.WriteLine("Введите другие числа");
+}
