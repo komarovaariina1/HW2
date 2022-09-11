c7d9a097d5c20cf77834812a42957e5594a10cf4
Arina <komarovaariina@mail.ru>
2022-09-11 15:53:14 +0300

    task11

---
+++ task11/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая вывод трехзначное число и удаляет вторую цифру этого числа
+// 436 -> 46
+// 782 -> 72
+
+int number = new Random().Next(100, 1000);
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 100 * 10;
+    int secondDigit = num % 10;
+    return firstDigit = secondDigit;
+}
+int result = MaxDigit(number);
+Console.WriteLine($"{number} -> {result}");
