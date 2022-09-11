f9bb293e73c3d69a8ca505800431a1c1e580723e
Arina <komarovaariina@mail.ru>
2022-09-11 17:04:05 +0300

    task16

---
+++ task16/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квардратом другого.
+
+Console.Write("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+bool square(int a1, int b1)
+{
+    return a1 * a1 ==b1 || b1 * b1 ==a1;
+
+}
+bool result = square(a, b);
+
+if (result) Console. WriteLine($"Правда");
+else Console.WriteLine($"Ложь");
